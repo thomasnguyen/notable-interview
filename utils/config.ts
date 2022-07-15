34e8d80bc8f226fabe78177c1d8e40cfbeba86ee
Thomas Nguyen <imtoshiru@gmail.com>
2022-07-15 22:47:59 +0000

    1 more time for the every1

--- utils/config.ts
+++ utils/config.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://notable-interviews-git-main-thomasnguyen.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://notable-interview-git-main-thomasnguyen.vercel.app';
